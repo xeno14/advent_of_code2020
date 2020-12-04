37727558339c8ef4484e93f63a3caf864bdc1422
xeno14 <integral14.dev@gmail.com>
2020-12-04 23:28:18 +0900

    filter and count

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,17 +4,13 @@ use regex::Regex;
 fn main() {
     // part1
     let ans = read_lines("input/day2.txt")
-        .filter(|line| {
-            is_valid(line.as_ref().unwrap())
-        })
+        .filter(|line| is_valid(line.as_ref().unwrap()))
         .count();
     println!("{:?}", ans);
 
     // part2
     let ans = read_lines("input/day2.txt")
-        .filter(|line| {
-            is_valid2(line.as_ref().unwrap())
-        })
+        .filter(|line| is_valid2(line.as_ref().unwrap()))
         .count();
     println!("{:?}", ans);
 }
@@ -24,24 +20,15 @@ fn is_valid(input: &str) -> bool {
     let re: Regex = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
     let caps = re.captures(input).unwrap();
 
-    let lbound: i32 = caps.get(1).map(|c| c.as_str().parse().unwrap()).unwrap();
-    let ubound: i32 = caps.get(2).map(|c| c.as_str().parse().unwrap()).unwrap();
-    let letter = caps.get(3).map(|c| c.as_str()).unwrap();
+    // TODO rewrite this
+    let lbound: usize = caps.get(1).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let ubound: usize = caps.get(2).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let letter = caps.get(3).map(|c| c.as_str()).unwrap().chars().nth(0).unwrap();
     let password = caps.get(4).map(|c| c.as_str()).unwrap();
 
-    // let count_letter = password.to_string().
-    let count_letter = password.chars().fold(0, |mut count, c| {
-        if c.to_string() == letter {
-            count += 1;
-        }
-        count
-    });
+    let count_letter = password.chars().filter(|c| c == &letter).count();
 
-    if lbound <= count_letter && count_letter <= ubound {
-        true
-    } else {
-        false
-    }
+    lbound <= count_letter && count_letter <= ubound
 }
 
 fn is_valid2(input: &str) -> bool {
