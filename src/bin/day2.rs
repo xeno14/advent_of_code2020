a82c4e3a7f6b9a7100c52f8498ef2a7bdf28d723
xeno14 <integral14.dev@gmail.com>
2020-12-04 21:44:58 +0900

    day2

---
+++ src/bin/day2.rs
@@ -0,0 +1,74 @@
+use regex::Regex;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let input = "input/day2.txt";
+    let mut ans = 0;
+    if let Ok(lines) = read_lines(input) {
+        for line in lines {
+            ans += if is_valid(&line.unwrap()) { 1 } else { 0 };
+        }
+    }
+    println!("{:?}", ans);
+
+    let input = "input/day2.txt";
+    let mut ans = 0;
+    if let Ok(lines) = read_lines(input) {
+        for line in lines {
+            ans += if is_valid2(&line.unwrap()) { 1 } else { 0 };
+        }
+    }
+    println!("{:?}", ans);
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn is_valid(input: &str) -> bool {
+    // extract password
+    let re: Regex = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let caps = re.captures(input).unwrap();
+
+    let lbound: i32 = caps.get(1).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let ubound: i32 = caps.get(2).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let letter = caps.get(3).map(|c| c.as_str()).unwrap();
+    let password = caps.get(4).map(|c| c.as_str()).unwrap();
+
+    // let count_letter = password.to_string().
+    let count_letter = password.chars().fold(0, |mut count, c| {
+        if c.to_string() == letter {
+            count += 1;
+        }
+        count
+    });
+
+    if lbound <= count_letter && count_letter <= ubound {
+        true
+    } else {
+        false
+    }
+}
+
+fn is_valid2(input: &str) -> bool {
+    // extract password
+    let re: Regex = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let caps = re.captures(input).unwrap();
+
+    let a: usize = caps.get(1).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let b: usize = caps.get(2).map(|c| c.as_str().parse().unwrap()).unwrap();
+    let letter = caps.get(3).map(|c| c.as_str()).unwrap().chars().nth(0).unwrap();
+    let password = caps.get(4).map(|c| c.as_str()).unwrap();
+
+    let cond1 = password.chars().nth(a-1).unwrap() == letter;
+    let cond2 = password.chars().nth(b-1).unwrap() == letter;
+    return cond1 ^ cond2;
+}
