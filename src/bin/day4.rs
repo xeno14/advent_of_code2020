4eb9de6f2a177ee0f097c49fac8556b382fa42d7
xeno14 <integral14.dev@gmail.com>
2020-12-05 21:36:56 +0900

    day4 lib

---
+++ src/bin/day4.rs
@@ -0,0 +1,196 @@
+use regex::Regex;
+use std::fs;
+use std::path::Path;
+
+use aoc::day4::*;
+
+fn main() {
+    let items = read_file(Path::new("input/day4-valid.txt"));
+    test(4, items);
+    let items = read_file(Path::new("input/day4-invalid.txt"));
+    test(0, items);
+    let items = read_file(Path::new("input/day4-invalid.txt"));
+    let items = read_file(Path::new("input/day4.txt"));
+    let ans = items
+        .into_iter()
+        .map(|item| validate_item(&item.to_string()))
+        .filter(|item| item.is_ok())
+        .count();
+    println!("{}", ans);
+}
+
+fn test(expected: usize, items: Vec<String>) {
+    type Res = Result<(), String>;
+    let (oks, errs): (Vec<Res>, Vec<Res>) = items
+        .into_iter()
+        .map(|item| validate_item(&item.to_string()))
+        .partition(Result::is_ok);
+    let ans = oks.into_iter().filter(Result::is_ok).count();
+    for err in errs.into_iter() {
+        println!("{}", err.unwrap_err());
+    }
+    if expected != ans {
+        panic!("test fialed");
+    }
+}
+
+pub fn extract_field(item: &str, field: &str) -> Result<String, String> {
+    let exp: String = format!(r"{}:([^\s]+)", field);
+
+    let caps = Regex::new(exp.as_ref())
+        .map_err(|e| e.to_string())?
+        .captures(item)
+        .ok_or(format!("unable to capture {}", exp))?;
+
+    let value = caps
+        .get(1)
+        .ok_or(format!("unable to capture {}", field))?
+        .as_str()
+        .to_string();
+    Ok(value)
+}
+
+fn validate_item(item: &str) -> Result<(), String> {
+    // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    let byr: String = extract_field(item, "byr")?;
+    validate_byr(&byr)?;
+
+    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    let iyr: String = extract_field(item, "iyr")?;
+    validate_iyr(&iyr);
+
+    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    let eyr: String = extract_field(item, "eyr")?;
+    if eyr.len() != 4 {
+        return Err(format!("eyr not 4 digit '{}'", eyr));
+    }
+    let ery: u32 = eyr.parse::<u32>().map_err(|e| e.to_string())?;
+    if !(2020 <= ery && ery <= 2030) {
+        return Err("ery out of range".to_owned());
+    }
+
+    // hgt (Height) - a number followed by either cm or in:
+    // If cm, the number must be at least 150 and at most 193.
+    // If in, the number must be at least 59 and at most 76.
+    let hgt: String = extract_field(item, "hgt")?;
+    match validate_height(&hgt) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    };
+
+    // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    let hcl: String = extract_field(item, "hcl")?;
+    match validate_haircolor(&hcl) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    };
+
+    // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    let valid_ecl: std::collections::HashSet<String> =
+        ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+            .iter()
+            .cloned()
+            .map(|s| s.to_string())
+            .collect();
+    let ecl: String = extract_field(item, "ecl")?;
+    if !valid_ecl.contains(&ecl) {
+        return Err("invalid ecl".to_owned());
+    }
+
+    // pid (Passport ID) - a nine-digit number, including leading zeroes.
+    let pid: String = extract_field(item, "pid")?;
+    match Regex::new("([0-9]{8})")
+        .map_err(|e| e.to_string())?
+        .captures(&pid)
+    {
+        Some(_) => (),
+        None => return Err("invalid pid".to_owned()),
+    };
+
+    // cid (Country ID) - ignored, missing or not.
+
+    Ok(())
+}
+
+fn validate_height(hgt: &str) -> Result<(), String> {
+    let expr = r"(\d+)([a-z]+)";
+
+    let caps = Regex::new(expr.as_ref())
+        .map_err(|e| e.to_string())?
+        .captures(hgt)
+        .ok_or(format!("unable to capture {}", expr))?;
+
+    let height = caps
+        .get(1)
+        .ok_or(format!("digit not found"))?
+        .as_str()
+        .parse::<u32>()
+        .map_err(|e| e.to_string())?;
+    let unit = caps
+        .get(2)
+        .ok_or(format!("unit not found"))?
+        .as_str()
+        .to_string();
+
+    // hgt (Height) - a number followed by either cm or in:
+    if unit != "cm" && unit != "in" {
+        return Err("wrong unit".to_owned());
+    }
+    // If cm, the number must be at least 150 and at most 193.
+    if unit == "cm" && !(150 <= height && height <= 193) {
+        return Err("invalid height for cm".to_owned());
+    }
+    // If in, the number must be at least 59 and at most 76.
+    else if unit == "in" && !(59 <= height && height <= 76) {
+        return Err("invalid height for in".to_owned());
+    }
+    Ok(())
+}
+
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+fn validate_haircolor(hcl: &str) -> Result<(), String> {
+    let expr = r"#([0-9a-z]{6})";
+
+    let caps = Regex::new(expr.as_ref())
+        .map_err(|e| e.to_string())?
+        .captures(hcl)
+        .ok_or(format!("unable to capture {}", expr))?;
+
+    let color = caps
+        .get(1)
+        .ok_or(format!("unable to capture {}", expr))?
+        .as_str();
+    if color.len() != 6 {
+        return Err("color is not 6digit".to_owned());
+    }
+    Ok(())
+}
+
+fn read_file(path: &Path) -> Vec<String> {
+    // std::fs::read_to_string
+    // https://doc.rust-lang.org/std/fs/fn.read_to_string.html
+    let input = match fs::read_to_string(path) {
+        Ok(input) => input,
+        Err(_) => panic!("input file missing"),
+    };
+    let input = input.trim();
+
+    let mut items: Vec<String> = Vec::new();
+    let mut iter = input.split("\n").into_iter();
+    while let Some(line) = iter.next() {
+        let mut item: String = line.trim().to_string();
+        if item.len() == 0 {
+            continue;
+        }
+        while let Some(line) = iter.next() {
+            let line = line.trim();
+            if line.len() == 0 {
+                break;
+            }
+            item.push(' ');
+            item.push_str(line);
+        }
+        items.push(item);
+    }
+    items
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,70 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-
 pub fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename).unwrap();
     io::BufReader::new(file).lines()
+}
+
+pub mod day4 {
+    pub type Res = Result<(), String>;
+
+    // validate year
+    fn validate_year(year: &str, min: u32, max: u32) -> Res {
+        if year.len() != 4 {
+            return Err("not 4 digits".to_owned());
+        }
+        let y: u32 = year.parse::<u32>().map_err(|e| e.to_string())?;
+        if y < min || max < y {
+            return Err("out of range".to_owned());
+        }
+        Ok(())
+    }
+
+    /// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    pub fn validate_byr(byr: &str) -> Res {
+        match validate_year(byr, 1920, 2002) {
+            Ok(ok) => Ok(ok),
+            Err(e) => Err(format!("{} {}", byr, e)),
+        }
+    }
+
+    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    pub fn validate_iyr(iyr: &str) -> Res {
+        match validate_year(iyr, 2010, 2020) {
+            Ok(ok) => Ok(ok),
+            Err(e) => Err(format!("{} {}", iyr, e)),
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_validate_byr() {
+            // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+            assert_eq!(Ok(()), validate_byr("2001"));
+            assert_eq!(Ok(()), validate_byr("2002"));
+            assert_eq!(Ok(()), validate_byr("1920"));
+
+            assert!(validate_byr("02001").is_err());
+            assert!(validate_byr("1919").is_err());
+            assert!(validate_byr("2003").is_err());
+        }
+
+        #[test]
+        fn test_validate_iyr() {
+            // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+            assert_eq!(Ok(()), validate_iyr("2010"));
+            assert_eq!(Ok(()), validate_iyr("2020"));
+
+            assert!(validate_iyr("02015").is_err());
+            assert!(validate_iyr("1919").is_err());
+            assert!(validate_iyr("2021").is_err());
+        }
+    }
 }
