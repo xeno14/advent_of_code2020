ac22d944988e6ac7468892819ec85e59dcbc71b3
xeno14 <integral14.dev@gmail.com>
2020-12-05 09:36:17 +0900

    day3 part1 functional

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -4,13 +4,21 @@ fn main() {
     // let input = "input/day3-example.txt";
     let input = "input/day3-1.txt";
     let lines: Vec<String> = read_lines(input).map(|line| line.unwrap()).collect();
-
-    let mut ans: usize = 0;
-    for (y, line) in lines.into_iter().enumerate() {
-        if y == 0 { continue }
-        let x = 3 * y % line.len();
-        let c = line.chars().nth(x).unwrap();
-        if c == '#' { ans += 1; continue }
-    }
+    let ans = part1(lines);
     println!("{}", ans);
+}
+
+fn part1(lines: Vec<String>) -> usize {
+    lines
+        .into_iter()
+        .enumerate()
+        .filter(|(y, line)| {
+            if *y == 0usize {
+                false
+            } else {
+                let x = 3 * y % line.len();
+                '#' == line.chars().nth(x).unwrap()
+            }
+        })
+        .count()
 }
