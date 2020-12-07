c37a4b3add639ec76532ebafd64b4fb7e198094d
xeno14 <integral14.dev@gmail.com>
2020-12-07 15:44:57 +0900

    day7 part1

--- python/day7.py
+++ python/day7.py
@@ -4,7 +4,7 @@ import re
 
 def parse_line(line: str):
     a = re.findall(r"([a-z]+ [a-z]+) bags contain", line)
-    b = re.findall(r"\d+ ([a-z]+ [a-z]+) bag", line)
+    b = re.findall(r"(\d)+ ([a-z]+ [a-z]+) bag", line)
     return a[0], b
 
 
@@ -12,9 +12,26 @@ def build_graph(lines: list):
     g = dict()
     for line in lines:
         u, l = parse_line(line)
-        assert(u!=g)
+        assert(not u in g)
+        l = [(int(a), b) for a, b in l]
         g[u] = l
     return g
+
+
+def solve_part1(g):
+    ok = set()  # reachable to shiny gold
+    goal = "shiny gold"
+
+    # returns true if the node is reachable to the goal
+    def rec(u) -> bool:
+        for _, v in g[u]:
+            if v in ok or v == goal or rec(v):
+                ok.add(u)
+                return True
+        return False
+    for u in g:
+        rec(u)
+    return len(ok)
 
 
 def main():
@@ -22,21 +39,10 @@ def main():
     filename = "input/day7.txt"
     with open(filename) as f:
         g = build_graph(f.readlines())
-    # print(g)
 
-    ok = set()  # reachable to shiny gold
-    goal = "shiny gold"
-
-    # returns true if the node is reachable to the goal
-    def rec(u) -> bool:
-        for v in g[u]:
-            if v in ok or v == goal or rec(v):
-                ok.add(u)
-                return True
-        return False
-    for u in g:
-        rec(u)
-    print(len(ok))
+    # part1
+    ans = solve_part1(g)
+    print(ans)
 
 
 if __name__ == "__main__":
