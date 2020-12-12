3484f060bdad7de346481707d2f93ae188ac8f32
xeno14 <integral14.dev@gmail.com>
2020-12-12 18:23:20 +0800

    day12 part2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -11,13 +11,13 @@ use std::str::FromStr;
 /// Action F means to move forward by the given value in the direction the ship is currently facing.
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Action {
-    North { value: u32 },
-    South { value: u32 },
-    East { value: u32 },
-    West { value: u32 },
-    Left { degree: u32 },
-    Right { degree: u32 },
-    Forward { value: u32 },
+    North { value: i32 },
+    South { value: i32 },
+    East { value: i32 },
+    West { value: i32 },
+    Left { degree: i32 },
+    Right { degree: i32 },
+    Forward { value: i32 },
 }
 
 impl FromStr for Action {
@@ -27,7 +27,7 @@ impl FromStr for Action {
         let re = Regex::new(r"([A-Z])(\d+)").unwrap();
         let caps = re.captures(s).unwrap();
         let direction = caps.get(1).unwrap().as_str();
-        let value: u32 = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
+        let value = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
         let a = match direction {
             "N" => Action::North { value },
             "S" => Action::South { value },
@@ -44,10 +44,15 @@ impl FromStr for Action {
 
 #[derive(Debug)]
 struct Ship {
+    // ship's position
     x: i64,
     y: i64,
+    // ship's direction
     u: i64,
     v: i64,
+    // relatrive positon to waypoint
+    wx: i64,
+    wy: i64,
 }
 
 impl Ship {
@@ -57,6 +62,8 @@ impl Ship {
             y: 0,
             u: 1,
             v: 0,
+            wx: 10,
+            wy: 1,
         }
     }
 
@@ -66,34 +73,79 @@ impl Ship {
                 self.y += value as i64;
             }
             Action::South { value } => {
-                self.y -= value as i64;
+                self.tick(Action::North { value: -value });
             }
             Action::East { value } => {
                 self.x += value as i64;
             }
             Action::West { value } => {
-                self.x -= value as i64;
+                self.tick(Action::East { value: -value });
             }
             Action::Left { degree } => {
-                let n = degree / 90;
+                let n = degree.abs() / 90;
+                let rotate = |u: i64, v: i64| {
+                    if degree.is_negative() {
+                        (v, -u)
+                    } else { 
+                        (-v, u)
+                    }
+                };
                 for _ in 0..n {
-                    let (u, v) = (-self.v, self.u);
+                    let (u, v) = rotate(self.u, self.v);
                     self.u = u;
                     self.v = v;
                 }
             }
             Action::Right { degree } => {
-                let n = degree / 90;
-                for _ in 0..n {
-                    let (u, v) = (self.v, -self.u);
-                    self.u = u;
-                    self.v = v;
-                    // (self.u, self.v) = (self.v, -self.u);
-                }
+                self.tick(Action::Left { degree: -degree });
             }
             Action::Forward { value } => {
                 self.x += self.u * value as i64;
                 self.y += self.v * value as i64;
+            }
+        }
+    }
+
+    /// delta of position: waypoint - ship
+    pub fn delta(&self) -> (i64, i64) {
+        (self.wx - self.x, self.wy - self.y)
+    }
+
+    pub fn tick2(&mut self, action: Action) {
+        match action {
+            Action::North { value } => {
+                self.wy += value as i64;
+            }
+            Action::South { value } => {
+                self.tick2(Action::North { value: -value });
+            }
+            Action::East { value } => {
+                self.wx += value as i64;
+            }
+            Action::West { value } => {
+                self.tick2(Action::East { value: -value });
+            }
+            Action::Left { degree } => {
+                let n = degree.abs() / 90;
+                let rotate = |u: i64, v: i64| {
+                    if degree.is_negative() {
+                        (v, -u)
+                    } else { 
+                        (-v, u)
+                    }
+                };
+                for _ in 0..n {
+                    let (u, v) = rotate(self.wx, self.wy);
+                    self.wx = u;
+                    self.wy = v;
+                }
+            }
+            Action::Right { degree } => {
+                self.tick2(Action::Left { degree: -degree });
+            }
+            Action::Forward { value } => {
+                self.x += self.wx * value as i64;
+                self.y += self.wy * value as i64;
             }
         }
     }
@@ -106,9 +158,19 @@ fn main() {
         .map(|line| line.unwrap().parse::<Action>())
         .filter_map(Result::ok)
         .collect();
+
+    // part1
     let mut ship = Ship::new();
-    for action in actions {
-        ship.tick(action);
+    for action in actions.iter() {
+        ship.tick(action.clone());
+        println!("{:?}", ship);
+    }
+    println!("{}", ship.x.abs() + ship.y.abs());
+
+    // part2
+    let mut ship = Ship::new();
+    for action in actions.iter() {
+        ship.tick2(action.clone());
         println!("{:?}", ship);
     }
     println!("{}", ship.x.abs() + ship.y.abs());
