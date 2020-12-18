a54ccac43c864055821185f66c132e0f1a2c4fe7
xeno14 <integral14.dev@gmail.com>
2020-12-18 18:40:56 +0800

    day18

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,4 +1,7 @@
+/// Recursive Descent Parsing to parse formula
+
 use aoc::read_lines;
+use std::iter::Peekable;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 enum Token {
@@ -6,7 +9,7 @@ enum Token {
     Close,
     Add,
     Mul,
-    Num(i64),
+    Num(u64),
 }
 
 impl Token {
@@ -17,7 +20,7 @@ impl Token {
             '+' => Token::Add,
             '*' => Token::Mul,
             c => {
-                let val: i64 = c.to_digit(10).unwrap() as i64;
+                let val: u64 = c.to_digit(10).unwrap() as u64;
                 Token::Num(val)
             }
         }
@@ -34,58 +37,98 @@ impl Token {
     }
 }
 
-/// expr = primary (op primary)*
-/// op = + | *
 /// primary = num | "(" expr ")"
-fn primary<'a, I>(tokens: &mut I) -> i64
+fn primary<'a, I>(tokens: &mut Peekable<I>) -> u64
 where
     I: Iterator<Item = &'a Token>,
 {
     match tokens.next() {
         Some(token) => match token {
             Token::Num(val) => *val,
-            Token::Open => expr(tokens),
-            _ => panic!("expect num or open"),
+            Token::Open => {
+                let num = expr(tokens);
+                tokens.next();  // take Close
+                num
+            }
+            _ => panic!()
         },
         None => panic!("expect token"),
     }
 }
 
-fn expr<'a, I>(tokens: &mut I) -> i64
+/// add = primary ("+" primary)*
+fn add<'a, I>(tokens: &mut Peekable<I>) -> u64
 where
     I: Iterator<Item = &'a Token>,
 {
     let mut num = primary(tokens);
-    while let Some(token) = tokens.next() {
+    while let Some(token) = tokens.peek() {
         let op = match token {
-            Token::Close => break,
-            Token::Add => |x: i64, y: i64| x + y,
-            Token::Mul => |x, y| x * y,
-            _ => panic!(format!("Unexpected token {:?}", token)),
+            Token::Add => {
+                tokens.next();
+                |x: u64, y: u64| x + y
+            }
+            _ => break,
         };
-        num = op(num, primary(tokens))
+        num = op(num, primary(tokens));
     }
     num
 }
 
-fn eval(s: String) -> i64 {
+/// expr = add ("*" add)*
+fn expr<'a, I>(tokens: &mut Peekable<I>) -> u64
+where
+    I: Iterator<Item = &'a Token>,
+{
+    let mut num = add(tokens);
+    while let Some(token) = tokens.peek() {
+        let op = match token {
+            Token::Mul => {
+                tokens.next();
+                |x, y| x * y
+            }
+            _ => break,
+        };
+        num = op(num, add(tokens))
+    }
+    num
+}
+
+fn eval(s: String) -> u64 {
     let tokens = Token::tokenize(s);
-    expr(&mut tokens.iter())
+    expr(&mut tokens.iter().peekable())
 }
 
 fn main() {
-    // part1
-    assert_eq!(26, eval("2 * 3 + (4 * 5)".to_owned()));
-    assert_eq!(437, eval("5 + (8 * 3 + 9 + 3 * 4 * 3)".to_owned()));
+    // // part1
+    // assert_eq!(26, eval("2 * 3 + (4 * 5)".to_owned()));
+    // assert_eq!(437, eval("5 + (8 * 3 + 9 + 3 * 4 * 3)".to_owned()));
+    // assert_eq!(
+    //     12240,
+    //     eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_owned())
+    // );
+    // assert_eq!(
+    //     13632,
+    //     eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_owned())
+    // );
+    // let ans: u64 = read_lines("input/day18.txt")
+    //     .map(|line| line.unwrap())
+    //     .map(eval)
+    //     .sum();
+    // println!("{}", ans);
+
+    // part2
+    assert_eq!(46, eval("2 * 3 + (4 * 5)".to_owned()));
+    assert_eq!(1445, eval("5 + (8 * 3 + 9 + 3 * 4 * 3)".to_owned()));
     assert_eq!(
-        12240,
+        669060,
         eval("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))".to_owned())
     );
     assert_eq!(
-        13632,
+        23340,
         eval("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2".to_owned())
     );
-    let ans: i64 = read_lines("input/day18.txt")
+    let ans: u64 = read_lines("input/day18.txt")
         .map(|line| line.unwrap())
         .map(eval)
         .sum();
