06564deba343f4b07b8931e4e8701f386875fbb3
xeno14 <1827523+xeno14@users.noreply.github.com>
2020-12-30 05:01:29 +0800

    day23 part2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,7 +1,9 @@
 use itertools::Itertools;
 
 fn parse(s: &str) -> Vec<usize> {
-    s.chars().map(|c| c.to_digit(10).unwrap() as usize).collect()
+    s.chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
 }
 
 fn main() {
@@ -10,11 +12,11 @@ fn main() {
     let input: Vec<usize> = parse(input_string);
 
     // 'next' represents a circular linked list
-    let mut next: Vec<usize> = vec![0; input.len()+1];
+    let mut next: Vec<usize> = vec![0; input.len() + 1];
     for (cur, nxt) in input.iter().tuple_windows() {
         next[*cur] = *nxt;
     }
-    next[input[input.len()-1]] = input[0];  // circulate
+    next[input[input.len() - 1]] = input[0]; // circulate
 
     // steps
     const MAX: usize = 9;
@@ -48,11 +50,23 @@ fn main() {
 }
 
 fn find_dst(cur: usize, pickup: &Vec<usize>, max: usize) -> usize {
-    let mut candidates : Vec<usize> = vec![0; 4];
-    candidates[0] = if cur==1 {max} else {cur-1};
-    candidates[1] = if candidates[0] == 1 {max} else {candidates[0]-1};
-    candidates[2] = if candidates[1] == 1 {max} else {candidates[1]-1};
-    candidates[3] = if candidates[2] == 1 {max} else {candidates[2]-1};
+    let mut candidates: Vec<usize> = vec![0; 4];
+    candidates[0] = if cur == 1 { max } else { cur - 1 };
+    candidates[1] = if candidates[0] == 1 {
+        max
+    } else {
+        candidates[0] - 1
+    };
+    candidates[2] = if candidates[1] == 1 {
+        max
+    } else {
+        candidates[1] - 1
+    };
+    candidates[3] = if candidates[2] == 1 {
+        max
+    } else {
+        candidates[2] - 1
+    };
 
     for c in candidates {
         if !pickup.contains(&c) {
@@ -65,7 +79,7 @@ fn find_dst(cur: usize, pickup: &Vec<usize>, max: usize) -> usize {
 fn print_cups(cur: usize, next: &Vec<usize>) {
     let mut c = cur;
     print!("{}", cur);
-    for _ in 0..(next.len()-2) {
+    for _ in 0..(next.len() - 2) {
         c = next[c];
         print!("{}", c);
     }
